2b123cb8f29a43ffef95ff09f9a05457a038f00e
임t <ssep4u@gmail.com>
2021-05-06 09:07:24 +0900

    bookmark/urls bookmark:detail

--- README.md
+++ README.md
@@ -24,3 +24,4 @@ models -> admin -> views -> templates -> urls
 - Bookmark Detail
     - bookmark/views BookmarkDetailView
     - bookmark/templates/bookmark bookmark_detail.html, bookmark_list.html
+    - bookmark/urls bookmark:detail
--- bookmark/urls.py
+++ bookmark/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import BookmarkListView, BookmarkCreateView
+from .views import BookmarkListView, BookmarkCreateView, BookmarkDetailView
 
 app_name = 'bookmark'
 
 urlpatterns = [
     path('', BookmarkListView.as_view(), name='list'),  # {% url 'bookmark:list' %}
     path('add/', BookmarkCreateView.as_view(), name='add'), # {% url 'bookmark:add' %}
+    path('detail/<int:pk>/', BookmarkDetailView.as_view(), name='detail'), # {% url 'bookmark:detail' pk=bookmark.pk %}
 ]
